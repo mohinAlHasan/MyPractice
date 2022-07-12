2c6c2cbbb0befd967a6a32e367b405725160baf3
mohinAlHasan <107579269+mohinAlHasan@users.noreply.github.com>
2022-07-12 20:41:26 +0600

    Add files via upload

---
+++ BitwiseOperator.java
@@ -0,0 +1,22 @@
+package JavaPractice;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		int a=32;
+		int b=12;
+		int c;
+		
+		c= a&b;
+		System.out.println("a & b= "+c);
+		
+		c= a|b;
+		System.out.println("a | b= "+c);
+		
+		c= a^b;
+		System.out.println("a ^ b= "+c);
+		
+		
+	}
+
+}
